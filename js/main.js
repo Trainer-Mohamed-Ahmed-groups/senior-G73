503a8d9299004ee4b282768977f39dcad4038484
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2024-02-03 14:42:26 +0200

    To Casting

--- js/main.js
+++ js/main.js
@@ -1,131 +1,70 @@
-"use strict"
-// If condition
+/*
+// Create a loop that display the names of people and order 
 
-var myGrade = 50;
+// Example : ["Mohamed","Ali","Khaled"]
+// Solution :
+// 1- Mohamed
+// 2- Ali
+// 3- Khaled
 
-if (myGrade === 500) {
-    console.log("OK")
-    if (5 > 4) console.log("Thank you")
-}
-else if (myGrade > 0) console.log(" GT 0")
-else {
-    console.log("Not OK");
-}
-
-
-
-/* ? ************************************************** Operators *************************************/
-
-/***********************! Arithmatic operators *****************/
-/**** Unary operators ****/
-
-var x = 5;
-
-console.log(x)
-
-
-
-console.log(++x)
-console.log(x++)
-console.log(x)
-console.log(--x)
-console.log(x--)
-
-/**** Binary operators ****/
-// + - * / 
-// %
-
-console.log(10 / 3)
-console.log(12 % 3)
-/***********************! Relational operators *****************/
-// > >= < <= 
-// == value
-// === value & data type
-
-console.log(5 == 5)
-console.log(5 != 5)
-
-
-console.log(5 === "5")
-console.log(5 !== "5")
-
-
-/***********************! Assignment operators *****************/
-// =
-// += -= *= /=
-var y = 4;
-console.log(y)
-y += 5;
-console.log(y)
-/***********************! Logical operators *****************/
-// && || !
-
-if (5 > 4 && 10 > 9) console.log("OK")
-else console.log("NO")
-
-
-if (5 > 4 || 10 > 90) console.log("OK")
-else console.log("NO")
-
-if (!false) console.log("NOOOOOOOO")
-
-/*********************************** Switch *********************************************/
-
-var t = 1;
-
-switch (t) {
-    case 1:
-        console.log("One")
-        break;
-    case 2:
-    case 3:
-        console.log("Two or Three")
-        break;
-    default:
-        console.log("Not above")
-        break;
-}
-
-/******************************************************* */
-
-// b = 7;
-
-// console.log(b)
-
-
-
-
-
-/********************************************** Loops ***********************************************/
-
-for (var i = 1; i <= 10; i++) {
-    console.log(i)
-}
-
-for (var i = 10; i > 0; i--) {
-    console.log(i)
-}
-
-
-var persons = ["Mohamed", "Mahmoud", "Omar", "Ahmed", "Ali"];
+var persons = ["Mohamed", "Ali", "Khaled"]
 
 for (let index = 0; index < persons.length; index++) {
     const element = persons[index];
-    console.log(element)
+    console.log(index + 1 + " - " + element)
 }
 
-var o = 1
-while (o <= 5) {
-    console.log(o)
-    o++
+
+
+// Create a loop that display the names of people that starts with m 
+// Example : ["Mohamed", "Ali", "Khaled" , "Mariem"]
+// Solution: Mohamed
+
+
+
+var persons2 = ["Mohamed", "Ali", "Khaled", "mariem"]
+
+for (let index = 0; index < persons2.length; index++) {
+    const element = persons2[index];
+    if (element[0] === 'M' || element[0] === 'm') {
+        console.log(element);
+    }
+}
+// NOTE : that will be easier with string methods in th future
+
+
+
+
+// Create a loop that give you the even numbers from 1 to 50
+
+for (let counter = 0; counter <= 50; counter += 2) {
+    if (counter != 0) console.log(counter)
+}
+console.log("------------------")
+
+for (let i = 0; i <= 50; i++) {
+    if (i % 2 === 0 &&  i !== 0) {
+        console.log(i)
+    }
 }
 
-console.log("*****************")
-var u = 1
+*/
 
-do {
-    console.log(u)
-    u++
-} while (u <= 5);
+// Create an array with numbers and function sum even numbers only
+
+var myNumbers = [1, 2, 3, 5, 4, 6, 8, 7, 9, 23, 4, 5, 6, 56, 45, 43, 342, 43],
+    sum = 0
+
+function evenFinder(num) {
+    if (num % 2 === 0 && num !== 0) return true
+}
+
+function setSum(num) { sum += num }
+
+for (let x = 0; x < myNumbers.length; x++) {
+    const element = myNumbers[x];
+    if (evenFinder(element)) { setSum(element) }
+}
 
 
+console.log(sum)
